6d36bd60f116705ae6df67a114fff88fc1f6aa84
ytwotap <1282490594@qq.com>
2021-10-28 00:33:13 +0800

    netty guide file

---
+++ src/main/java/com/yt/netty/DiscardServerHandler.java
@@ -0,0 +1,39 @@
+package com.yt.netty;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel.
+ * Discard server implement .
+ *
+ */
+public class DiscardServerHandler extends ChannelHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        ByteBuf in=(ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.println((char) in.readByte());
+                //刷新缓冲流
+                System.out.flush();
+            }
+        }finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+//        // Discard the received data silently.
+//        ((ByteBuf) msg).release(); // (3)
+//        System.out.println("invoke channelRead()");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/yt/netty/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package com.yt.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel.
+ * echo service implement
+ *
+ */
+public class EchoServerHandler extends ChannelHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        //return msg
+        ctx.write(msg);//(1)
+
+        ctx.flush();//(2)
+
+
+        //can use next
+//        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
